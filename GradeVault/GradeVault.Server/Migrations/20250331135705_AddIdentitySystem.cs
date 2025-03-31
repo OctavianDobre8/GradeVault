776a182a87d1d9dda103827fe0fefef469938fd9
Robique <robert-cristian.lungu@student.unitbv.ro>
2025-03-31 18:43:41 +0300

    Finished the auth system

---
+++ GradeVault/GradeVault.Server/Migrations/20250331135705_AddIdentitySystem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GradeVault.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentitySystem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Teachers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Teachers");
+        }
+    }
+}
